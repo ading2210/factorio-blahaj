743c187b60e4627ca06bca7da5ee7386051b4284
ading2210 <ading2019@gmail.com>
2024-01-27 17:11:01 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BLAHAJ Factorio Mod
+
+This a [Factorio](https://factorio.com/) mod that replaces all fish with [BLAHAJ](https://en.wikipedia.org/wiki/Bl%C3%A5haj).
---
+++ data.lua
@@ -0,0 +1,47 @@
+data.raw["capsule"]["raw-fish"].localised_name = "BLAHAJ"
+data.raw["capsule"]["raw-fish"].localised_description = "A cute BLAHAJ. Left click to cuddle with it and gain some health."
+
+data.raw["capsule"]["raw-fish"].icon = "__blahaj__/graphics/blahaj_full.png"
+data.raw["capsule"]["raw-fish"].icon_size = 512
+data.raw["capsule"]["raw-fish"].capsule_action = {
+  type = "use-on-self",
+  uses_stack = false,
+  attack_parameters = {
+    type = "projectile",
+    activation_type = "activate",
+    range = 0,
+    cooldown = 120,
+    ammo_type = {
+      category = "capsule",
+      target_type = "position",
+      action = {
+        type = "direct",
+        action_delivery = {
+          type = "instant",
+          target_effects = {
+            {
+              type = "damage",
+              damage = {
+                amount = -20,
+                type = "physical"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+data.raw["fish"]["fish"].localised_name = "BLAHAJ"
+data.raw["fish"]["fish"].icon = "__blahaj__/graphics/blahaj_full.png"
+data.raw["fish"]["fish"].icon_size = 512
+data.raw["fish"]["fish"].pictures = {
+  {
+    filename = "__blahaj__/graphics/blahaj_entity.png",
+    priority = "extra-high",
+    width = 32,
+    height = 32
+  }
+}
